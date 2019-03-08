cbdfa9a0c37857ee01bfc33f7be157b24e14dc73
onyul <heogeuns@gmail.com>
2019-03-08 18:18:15 +0900

    git modify i Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,31 +1,4 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
-=======
 import PhoneForm from './components/PhoneForm.js';
 import PhoneInfoList from './components/PhoneInfoList.js';
 
@@ -65,6 +38,5 @@ class App extends Component {
         );
     }
 } 
->>>>>>> 0f964f69f9ff6a29f153d7bc79489eb9860e8076
 
 export default App;
